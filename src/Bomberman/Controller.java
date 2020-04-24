b37700f149744e6bada6ac175efe33c8af6ba851
Bence <laszbence96@gmail.com>
2020-04-24 17:21:18 +0200

    First version of main GUI windows.

---
+++ src/Bomberman/Controller.java
@@ -0,0 +1,103 @@
+package Bomberman;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+public class Controller implements Initializable {
+
+    @FXML
+    private AnchorPane mainmenu_pane;
+
+    @FXML
+    private Button button_startonlinegame;
+
+    @FXML
+    private Label label_waitingconnection;
+
+    @FXML
+    private Label label_IPAdress;
+
+    @FXML
+    private void connected_action(ActionEvent event){
+        label_waitingconnection.setText("Connected");
+        button_startonlinegame.setVisible(true);
+    }
+
+    @FXML
+    private void startonline_action(ActionEvent event){
+        System.out.println("Starting the online game...");
+    }
+
+    @FXML
+    private void newonline_action(ActionEvent event) throws IOException
+    {
+        System.out.println("Starting new online game...");
+        Parent newOnlineGameParent = FXMLLoader.load(getClass().getResource("NewOnlineGame.fxml"));
+        Scene newOnlineGameScene = new Scene(newOnlineGameParent);
+
+        Stage window = (Stage) ((Node)event.getSource()).getScene().getWindow();
+        window.setScene(newOnlineGameScene);
+        window.show();
+    }
+
+    @FXML
+    private void newoffline_action(ActionEvent event) throws IOException{
+        System.out.println("Starting new offline game...");
+        Parent newOnlineGameParent = FXMLLoader.load(getClass().getResource("NewOfflineGame.fxml"));
+        Scene newOnlineGameScene = new Scene(newOnlineGameParent);
+
+        Stage window = (Stage) ((Node)event.getSource()).getScene().getWindow();
+        window.setScene(newOnlineGameScene);
+        window.show();
+    }
+
+    @FXML
+    private void connectonline_action(ActionEvent event) throws IOException{
+        System.out.println("Connecting to online game...");
+        Parent newOnlineGameParent = FXMLLoader.load(getClass().getResource("ConnectOnlineGame.fxml"));
+        Scene newOnlineGameScene = new Scene(newOnlineGameParent);
+
+        Stage window = (Stage) ((Node)event.getSource()).getScene().getWindow();
+        window.setScene(newOnlineGameScene);
+        window.show();
+    }
+
+    @FXML
+    private void viewranglist_action(ActionEvent event) throws IOException{
+        System.out.println("Loading ranglist...");
+        Parent newOnlineGameParent = FXMLLoader.load(getClass().getResource("Ranglist.fxml"));
+        Scene newOnlineGameScene = new Scene(newOnlineGameParent);
+
+        Stage window = (Stage) ((Node)event.getSource()).getScene().getWindow();
+        window.setScene(newOnlineGameScene);
+        window.show();
+    }
+
+    @FXML
+    private void backtomain_action(ActionEvent event) throws IOException{
+        System.out.println("Back to main menu...");
+        Parent newOnlineGameParent = FXMLLoader.load(getClass().getResource("mainmenu.fxml"));
+        Scene newOnlineGameScene = new Scene(newOnlineGameParent);
+
+        Stage window = (Stage) ((Node)event.getSource()).getScene().getWindow();
+        window.setScene(newOnlineGameScene);
+        window.show();
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
---
+++ src/Bomberman/Main.java
@@ -0,0 +1,24 @@
+package Bomberman;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("mainmenu.fxml"));
+        primaryStage.setTitle("Bomberman");
+        primaryStage.setScene(new Scene(root, 800, 600));
+        primaryStage.setResizable(false);
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
