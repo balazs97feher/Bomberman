4f582ae412dd9896188762827e8d7aa2b80652c9
FBalazs <balazs97feher@gmail.com>
2020-05-03 11:40:07 +0200

    Work on logic skeleton: game and level initialization Add EquivalentPlayerNamesException class

--- src/gameplay/logic/Game.java
+++ src/gameplay/logic/Game.java
@@ -1,6 +1,7 @@
 package gameplay.logic;
 
 import gameplay.grid.*;
+import gameplay.logic.exception.EquivalentPlayerNamesException;
 import gameplay.logic.gridfactory.GridFactory;
 
 import java.util.ArrayList;
@@ -13,27 +14,82 @@ public class Game {
     protected ArrayList<Bomb> bombs;
     protected Grid grid;
 
+    private int level;
+
     final int gridLength = 15;
     final int gridWidth = 21;
 
     private GridFactory gridFactory;
     private Controller controller;
+    private boolean isRunning;
 
     public Game(){
         gridFactory = new GridFactory(gridLength, gridWidth);
         controller = new Controller(this);
+        level = 0;
+        isRunning = false;
     }
 
-    public void initialize(ArrayList<String> players){
+    /**
+     * should be called right after a game is constructed
+     * @param playerNames non-empty list of the names of the players
+     * @throws EquivalentPlayerNamesException
+     */
+    public void initialize(ArrayList<String> playerNames) throws EquivalentPlayerNamesException {
+        playerScores = new HashMap<>(playerNames.size());
+        for (String name : playerNames){
+            if(playerScores.containsKey(name)) throw new EquivalentPlayerNamesException("There is a player called " + name + " already!");
+            else playerScores.put(name, 0);
+        }
+
+        run();
+    }
+
+    private void initializeNextLevel(){
+        level++;
+        grid = gridFactory.makeUniformGrid();
+
+        populateGridWithPlayers();
+        populateGridWithMonsters();
+    }
+
+    private void populateGridWithPlayers(){
+
+    }
+
+    private void populateGridWithMonsters(){
 
     }
 
 
+    private void run(){
+        isRunning = true;
+        while(isRunning){
+            initializeNextLevel();
 
+
+
+
+
+        }
+
+
+    }
+
+
+    /**
+     * function to move a player on the grid
+     * @param name name of the player to  move
+     * @param dir in which direction to move
+     */
     public void movePlayer(String name, Direction dir){
         // TODO
     }
 
+    /**
+     * function to place a bomb where the player currently is
+     * @param name name of the player to place the bomb
+     */
     public void placeBomb(String name){
         // TODO
     }
---
+++ src/gameplay/logic/exception/EquivalentPlayerNamesException.java
@@ -0,0 +1,7 @@
+package gameplay.logic.exception;
+
+public class EquivalentPlayerNamesException extends RuntimeException{
+    public EquivalentPlayerNamesException(String message){
+        super(message);
+    }
+}
